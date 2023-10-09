5b4f6fe23191162e7fb9e3f4c58db5193ff296bf
eecmanny <eec123manny@gmail.com>
2023-10-09 06:08:21 -0400

    routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,28 +29,28 @@ router.get('/', async (req, res) => {
 });
 
 
-//http://localhost:3001/dashboard/1
-router.get('/dashboard/:id', async (req, res) => {
-  try {
-    const blogData = await Blog.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+// //http://localhost:3001/dashboard/1
+// router.get('/dashboard/:id', async (req, res) => {
+//   try {
+//     const blogData = await Blog.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
-    const blog = blogData.get({ plain: true });
+//     const blog = blogData.get({ plain: true });
 
-    res.render('dashboard', {
-      ...blog,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('dashboard', {
+//       ...blog,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 //http://localhost:3001/blog
 router.get('/dashboard', withAuth, async (req, res) => {
@@ -66,6 +66,54 @@ router.get('/dashboard', withAuth, async (req, res) => {
     res.render('dashboard', {
       ...user,
       logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Why dosnt this code work
+// //http://localhost:3001/techPost
+// router.get('/techPost', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: blog }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('createBlog', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+//http://localhost:3001/techPost
+
+router.get('/techPost', async (req, res) => {
+  try {
+    // Get all Blogs and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('createBlog', { 
+      blogs, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
